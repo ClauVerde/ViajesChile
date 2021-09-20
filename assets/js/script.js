1f666c1316276b97af97d632e490483294f14e78
ClauVerde <90655863+ClauVerde@users.noreply.github.com>
2021-09-20 02:41:19 -0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,33 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        $('html,body').animate({
+            'scrollTop': $($(this).attr('href')).offset().top+'px'
+        }, 3000);
+        
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+
+
 $(function () {
     $('[data-toggle="tooltip"]').tooltip();
 
-    $('#somos').click(function(){
-        alert("Somos la mejor empresa de viajes")
-    });
+});
 
-    $('#destacado').click(function(){
-        alert("Comparte en los mejores lugares de Chile")
-    });
-    
-    $('#contactos').click(function(){
-        alert("Que esperas para ser parte de nosotros?")
-    });
+      
+
+
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll < 300) {
+      $(".fixed-top").css("background", "transparent");
+    } else {
+      $(".fixed-top").css("background", "#15aabf");
+    }
+});
+
