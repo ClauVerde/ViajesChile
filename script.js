5de0a99d3d021f64a3d483d160c271817211a142
ClauVerde <90655863+ClauVerde@users.noreply.github.com>
2021-09-20 01:39:53 -0300

    Tercer Commit
    Se incorporan efectos de scroll y ordenan cards

---
+++ script.js
@@ -0,0 +1,45 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        $('html,body').animate({
+            'scrollTop': $($(this).attr('href')).offset().top+'px'
+        }, 3000);
+        
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+
+    $('#somos').click(function(){
+        alert("Tu destino en Chile")
+    });
+
+    $('#destacado').click(function(){
+        alert("Tu destino en Chile")
+    });
+    
+    $('#contactos').click(function(){
+        alert("Tu destino en Chile")
+    });
+});
+
+      
+
+
+
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll < 300) {
+      $(".fixed-top").css("background", "transparent");
+    } else {
+      $(".fixed-top").css("background", "#15aabf");
+    }
+});
+
